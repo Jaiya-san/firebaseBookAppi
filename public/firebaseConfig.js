45d00490b0fb6b43c5e389ed593147291fe32022
Jaiya-san <jaiyajittan@gmail.com>
2026-04-19 16:31:05 -0400

    initalize firebase

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,11 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBnyTSJNB9SOsx1-RPySp7OOzEOK06rM0c",
-    authDomain: "info1601-booksapp.firebaseapp.com",
-    projectId: "info1601-booksapp",
-    storageBucket: "info1601-booksapp.firebasestorage.app",
-    messagingSenderId: "568459881258",
-    appId: "1:568459881258:web:a37d518df9686e77120c53"
-  };
+  apiKey: "AIzaSyC5rz9dAx5CaA5jnj6CV5knZmCn1Km-5V8",
+  authDomain: "booksapp-fc757.firebaseapp.com",
+  projectId: "booksapp-fc757",
+  storageBucket: "booksapp-fc757.firebasestorage.app",
+  messagingSenderId: "1039113012540",
+  appId: "1:1039113012540:web:310c39377f1e56c0d6ca39"
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  export default firebaseConfig;
+export default firebaseConfig;
